d770a2e1e9f7733fb0e03ad5a6b87185f62d3de2
Kai Hartzell <kai.hartzell@gmail.com>
2019-11-05 14:45:56 +0200

    exercise 9

--- viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -2,14 +2,22 @@ package ohtu;
 
 import ohtu.verkkokauppa.Kauppa;
 import ohtu.verkkokauppa.Kirjanpito;
+import ohtu.verkkokauppa.Pankki;
+import ohtu.verkkokauppa.Varasto;
+import ohtu.verkkokauppa.Viitegeneraattori;
 
 public class Main {
 
     public static void main(String[] args) {
-        Kauppa kauppa = new Kauppa();
+        Kauppa kauppa = new Kauppa(
+                Varasto.getInstance(),
+                Pankki.getInstance(),
+                Viitegeneraattori.getInstance()
+        );
 
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
         kauppa.lisaaKoriin(1);
         kauppa.lisaaKoriin(3);
         kauppa.lisaaKoriin(3);
