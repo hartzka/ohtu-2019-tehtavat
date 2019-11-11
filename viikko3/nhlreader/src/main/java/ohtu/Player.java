392e995c748ad8ca81ddfcc2c25d859a36665abd
Kai Hartzell <kai.hartzell@gmail.com>
2019-11-11 16:37:36 +0200

    wee3.1

---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,57 @@
+
+package ohtu;
+
+public class Player {
+    private String name;
+    private String nationality;
+    private String team;
+    private int goals;
+    private int assists;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + team + " " + 
+        Integer.toString(goals) + "+" + Integer.toString(assists);
+    }
+      
+}
